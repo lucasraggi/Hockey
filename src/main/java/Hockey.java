b6b1c97f3fa3902269fc0767429da81102e43600
lucas <lucas.raggi@hotmail.com>
2019-02-05 23:23:48 -0200

    Lines corrected Jframe integrated

--- src/main/java/Hockey.java
+++ src/main/java/Hockey.java
@@ -6,23 +6,60 @@ import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.FPSAnimator;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
+import java.util.ArrayList;
 
 import static com.jogamp.opengl.GL.*;
 
-public class Hockey extends GLCanvas implements GLEventListener {
+public class Hockey extends GLCanvas implements GLEventListener, MouseListener, MouseMotionListener, ActionListener {
 
     private static String TITLE = "JOGL 2.0 Setup (GLCanvas)";  // window's title
-    private static final int CANVAS_WIDTH = 640;  // width of the drawable
+    private static final int CANVAS_WIDTH = 1300;  // width of the drawable
     private static final int CANVAS_HEIGHT = 780; // height of the drawable
     private static final int FPS = 60; // animator's target frames per second
+    private static int firstClickX = -1;
+    private static int firstClickY = -1;
+    private static final int TICK_MIN = 0;
+    private static final int TICK_INIT = 2;
+    private static final int TICK_MAX = 10;
+    private ArrayList<Line> lines = new ArrayList<>();
     private Drawer dw = new Bresenham();
     private GLU glu;
 
+    
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        y = Math.abs(y - CANVAS_HEIGHT);
+
+
+
+
+        if(firstClickX == -1 && firstClickY == -1){
+            firstClickX = x;
+            firstClickY = y;
+        }
+        else {
+            lines.add(new Line(firstClickX, firstClickY, x, y));
+            //System.out.println("Received: x1: "+ firstClickX + " y1:" + firstClickY + " x2:" + x + " y2:" + y);
+            firstClickX = -1;
+            firstClickY = -1;
+
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+
     public static void main(String[] args) {
+
         // Run the GUI codes in the event-dispatching thread for thread safety
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -36,7 +73,8 @@ public class Hockey extends GLCanvas implements GLEventListener {
 
                 // Create the top-level container
                 final JFrame frame = new JFrame(); // Swing's JFrame or AWT's Frame
-                frame.getContentPane().add(canvas);
+
+
                 frame.addWindowListener(new WindowAdapter() {
                     @Override
                     public void windowClosing(WindowEvent e) {
@@ -51,7 +89,77 @@ public class Hockey extends GLCanvas implements GLEventListener {
                         }.start();
                     }
                 });
+                JPanel p = new JPanel();
+                p.setLayout(new FlowLayout());
                 frame.setTitle(TITLE);
+                JPanel botoes = new JPanel();
+
+                JRadioButton retaButton = new JRadioButton("Naive");
+                JRadioButton bressButton = new JRadioButton("Bresenham");
+
+                retaButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if(retaButton.isSelected()){
+
+                        }
+                    }
+                });
+
+                bressButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if(bressButton.isSelected()){
+
+                        }
+                    }
+                });
+
+                retaButton.setSelected(true);
+
+                JSlider espessura = new JSlider(JSlider.HORIZONTAL, TICK_MIN, TICK_MAX, TICK_INIT);
+
+                espessura.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+
+                    }
+                });
+
+                JLabel lOpcao = new JLabel("Algoritmo");
+                JLabel lEspessura = new JLabel("Espessura");
+
+                ButtonGroup buttonGroup = new ButtonGroup();
+                buttonGroup.add(retaButton);
+                buttonGroup.add(bressButton);
+
+                JButton botaoCor = new JButton("Cor");
+                botaoCor.setOpaque(true);
+                botaoCor.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Color color = JColorChooser.showDialog(null, "Escolha a cor para as retas", Color.BLUE);
+                        System.out.println(color);
+
+                        if(color != null) {
+                            botaoCor.setBackground(color);
+                        }
+                    }
+                });
+
+
+                botoes.setLayout(new BoxLayout(botoes, BoxLayout.Y_AXIS));
+                botoes.add(botaoCor);
+                botoes.add(lOpcao);
+                botoes.add(retaButton);
+                botoes.add(bressButton);
+                botoes.add(lEspessura);
+                botoes.add(espessura);
+
+
+                Container pane = frame.getContentPane();
+                pane.add(botoes, BorderLayout.LINE_START);
+                pane.add(canvas);
                 frame.pack();
                 frame.setVisible(true);
                 animator.start(); // start the animation loop
@@ -61,6 +169,7 @@ public class Hockey extends GLCanvas implements GLEventListener {
 
     public Hockey() {
         this.addGLEventListener(this);
+        addMouseListener(this);
     }
 
     @Override
@@ -89,6 +198,15 @@ public class Hockey extends GLCanvas implements GLEventListener {
         *  y2 = 750
         *
         * */
+        Line debug = new Line(0,0,0,0);
+        for (Line line : lines) {
+            dw.drawLine(line.x1, line.y1, line.x2, line.y2, gl);
+            debug = line;
+        }
+        //if (firstClickX != -1)
+            //System.out.println("Processed: x1: "+ debug.x1 + " y1:" + debug.y1 + " x2:" +debug.x2 + " y2:" + debug.y2);
+
+
 
         // Edges
         dw.drawLine(100, 100, 100, 700, gl);
@@ -156,4 +274,31 @@ public class Hockey extends GLCanvas implements GLEventListener {
 
     @Override
     public void dispose(GLAutoDrawable drawable) { }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseDragged(MouseEvent e)
+    {
+        // update the label to show the point
+        // through which point mouse is dragged
+    }
+
+    public void mouseMoved(MouseEvent e)
+    {
+    }
 }
+
